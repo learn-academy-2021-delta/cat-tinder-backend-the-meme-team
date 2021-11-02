7d4ff0317210a91da5d103326783b5b45c4d6657
youngmarisa <marisayoung2013@gmail.com>
2021-11-02 14:55:07 -0700

    validations

--- spec/models/meme_spec.rb
+++ spec/models/meme_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Meme, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have a valid name" do
+    meme = Meme.create(url: 'www.google.com', description: 'Kevin meme')
+    expect(meme.errors[:name].first).to eq "can't be blank"
+  end
+  it 'should have a valid url' do
+    meme = Meme.create(name: 'Kevin', description: 'Kevin Meme')
+    expect(meme.errors[:url].first).to eq "can't be blank"
+  end
+  it 'should have a valid description' do
+    meme = Meme.create(name: 'Kevin', url: 'www.google.com')
+    expect(meme.errors[:description].first).to eq "can't be blank"
+  end
+  it 'should have a description entry of at least 10 characters' do
+    meme = Meme.create(name: 'Kevin', url: 'www.google.com', description:'Kevin')
+    expect(meme.errors[:description].first).to eq 'is too short (minimum is 10 characters)'
+  end
 end
--- spec/requests/memes_spec.rb
+++ spec/requests/memes_spec.rb
@@ -73,12 +73,148 @@ RSpec.describe "Memes", type: :request do
       }
       post '/memes', params: meme_params
       meme = Meme.first
+      p Meme.errors
       delete "/memes/#{meme.id}"
       expect(response).to have_http_status(200)
       memes = Meme.all
       expect(memes).to be_empty
     end
   end
+
+  describe 'meme validation error codes' do
+    it 'does not create a meme without a name' do
+      meme_params = {
+        meme: {
+          url: 'google.com',
+          description: 'Kevin Meme'
+        }
+      }
+      post '/memes', params: meme_params
+      expect(response).to have_http_status(422)
+      meme = JSON.parse(response.body)
+      expect(meme['name']).to include "can't be blank"
+    end
+    it 'does not create a meme without a url' do
+      meme_params = {
+        meme: {
+          name: 'Kevin',
+          description: 'Kevin Meme'
+        }
+      }
+      post '/memes', params: meme_params
+      expect(response).to have_http_status(422)
+      meme = JSON.parse(response.body)
+      expect(meme['url']).to include "can't be blank"
+    end
+    it 'does not create a meme without a description' do
+      meme_params = {
+        meme: {
+          name: 'Kevin',
+          url: 'google.com'
+        }
+      }
+      post '/memes', params: meme_params
+      expect(response).to have_http_status(422)
+      meme = JSON.parse(response.body)
+      expect(meme['description']).to include "can't be blank"
+    end
+  end
+
+  describe "cannot update a meme without valid attributes" do
+    it 'cannot update a meme without a name' do
+      meme_params = {
+        meme: {
+          name: 'Rob',
+          url: 'google.com',
+          description: 'Roberts meme'
+        }
+      }
+      post '/memes', params: meme_params
+      old_meme = Meme.first
+      # p Meme.all
+      meme_params = {
+        meme: {
+          name: '',
+          url: 'google.com',
+          description: 'Roberts meme'
+        }
+      }
+      
+      patch "/memes/#{old_meme.id}", params: meme_params
+      meme = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+      expect(meme['name']).to include "can't be blank"
+    end
+
+    it 'cannot update a meme without a url' do
+      meme_params = {
+        meme: {
+          name: 'Rob',
+          url: 'google.com',
+          description: 'Roberts meme'
+        }
+      }
+      post '/memes', params: meme_params
+      meme = Meme.first
+      meme_params = {
+        meme: {
+          name: 'Rob',
+          url: '',
+          description: 'Roberts meme'
+        }
+      }
+      patch "/memes/#{meme.id}", params: meme_params
+      meme = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+      expect(meme['url']).to include "can't be blank"
+    end
+
+    it 'cannot update a meme without an description' do
+      meme_params = {
+        meme: {
+          name: 'Rob',
+          url: 'google.com',
+          description: 'Roberts meme'
+        }
+      }
+      post '/memes', params: meme_params
+      meme = Meme.first
+      meme_params = {
+        meme: {
+          name: 'Rob',
+          url: 'google.com',
+          description: '',
+        }
+      }
+      patch "/memes/#{meme.id}", params: meme_params
+      meme = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+      expect(meme['description']).to include "can't be blank"
+    end
+    it 'cannot update a meme without a description that is at least 10 characters' do
+      meme_params = {
+        meme: {
+          name: 'Rob',
+          url: 'google.com',
+          description: 'ricky bobby meme'
+        }
+      }
+      post '/memes', params: meme_params
+      meme = Meme.first
+      meme_params = {
+        meme: {
+          name: 'Rob',
+          url: 'google.com',
+          description: 'meme'
+        }
+      }
+      patch "/memes/#{meme.id}", params: meme_params
+      meme = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+      expect(meme['description']).to include 'is too short (minimum is 10 characters)'
+    end
+  end
+
 
 
 
